55bf5b4c3b538992b40ba65166fce99faeabd57a
Deivisson <deivisson14czs@gmail.com>
2025-03-20 23:34:12 -0300

    mudança basico no js

--- HtmlPrincipal/index.js
+++ HtmlPrincipal/index.js
@@ -1,17 +1,16 @@
 let pedidos = [];
 
         function registrarPedido() {
-            let nome = document.getElementById('nome').value;
-            let sobrenome = document.getElementById('sobrenome').value;
-            let turno = document.getElementById('turno').value;
-            let pedido = document.getElementById('pedido').value;
+            let nome = document.getElementById('Nome').value;
+            let codigo = document.getElementById('Codigo').value;
+            let quantidade = document.getElementById('Quantidade').value;
 
-            if (!nome || !sobrenome || !pedido) {
+            if (!nome || !codigo || !pedido) {
                 alert('Preencha todos os campos!');
                 return;
             }
 
-            pedidos.push({ Nome: nome, Sobrenome: sobrenome, Turno: turno, Pedido: pedido });
+            pedidos.push({ Nome: nome, Codigo: codigo, Quantidade: quantidade });
             alert('Pedido registrado com sucesso!');
             document.getElementById('pedidoForm').reset();
         }
