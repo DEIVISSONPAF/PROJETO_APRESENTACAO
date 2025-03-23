fda529878318138397ab9f8f8eefb8373957aef8
Deivisson <deivisson14czs@gmail.com>
2025-03-23 00:21:53 -0300

    sitye

--- index.js
+++ index.js
@@ -2,7 +2,3 @@
 function abrirNovaAba() {
     window.open('https://docs.google.com/spreadsheets/d/1jbI9wN9ny8HCJPOX66i69zdCSp6eoHNK9V5IhJ5ftMk/edit?gid=0#gid=0', '_blank');
 }
-
-function FicarNoSite() {
-    window.open('https://projetoapresentaca.netlify.app/', '_blank');
-}
